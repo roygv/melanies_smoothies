72a30844feca26517762c2d8758fce8425059330
Roy Gvirtsman <roygv@users.noreply.github.com>
2025-02-07 13:31:21 -0800

    Update streamlit_app.py
    Add pandas and test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
@@ -18,7 +19,9 @@ st.write('The name on your smootie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+# st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 # option = st.selectbox(
 #     "What is your favorite fruit?",
