8a70736e25eeb875f63c69d4842464bfb6cc4e6f
Roy Gvirtsman <roygv@users.noreply.github.com>
2025-02-07 13:37:18 -0800

    Update streamlit_app.py
    Search on the search_on field

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+# st.dataframe(pd_df)
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 # option = st.selectbox(
@@ -40,7 +40,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + " "
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
     my_insert_stmt = """ insert into smoothies.public.orders
